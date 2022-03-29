144f9a155f9a1cf5ba4761b25a8f2b4d30e6ae4e
Lars Vilhuber <lars@vilhuber.com>
2022-03-29 11:55:00 -0400

    Re #3 a few updates/streamlining

---
+++ tools/check_rds_files.R
@@ -0,0 +1,84 @@
+# Author: Andres Aradillas Fernandez
+# (C) 2022
+# Licensed under BSD-3 license
+#
+# Running instructions: Run all the code at once, no manual changes needed.
+
+# Import necessary packages, install if necessary
+
+pkgTest <- function(x,y="")
+{
+	if (!require(x,character.only = TRUE))
+	{
+		if ( y == "" ) 
+			{
+		        install.packages(x,dep=TRUE)
+			} else {
+			remotes::install_version(x, y)
+			}
+		if(!require(x,character.only = TRUE)) stop("Package not found")
+	}
+	return("OK")
+}
+
+global.libraries <- c("here")
+global.libraries <- c("here","stringi")
+
+results <- sapply(as.list(global.libraries), pkgTest)
+
+# create basename
+basename <- "r-data-checks"
+
+# Finds current directory
+root <- here()
+
+# Create list of all RDS files in directory
+datafiles_list <- list.files(root, pattern = "\\.rds", full.names = TRUE, recursive = TRUE, ignore.case = TRUE, include.dirs = FALSE)
+
+# Create list denoting successes/failures
+read_success <- list()
+
+# Loop to read all RDS files, recording successes as "yes"
+if ( length(datafiles_list) == 0 ) {
+  message("No RDS files found, exiting")
+} else {
+  for(k in 1:length(datafiles_list)){
+    t <- try(readRDS(datafiles_list[[k]]))
+    if ("try-error" %in% class(t)){
+      read_success[[k]] <- "No"
+    } else{
+      read_success[[k]] <- "Yes"
+    }
+  }
+
+  # Convert list into data frame
+  df <- data.frame(matrix(unlist(datafiles_list), nrow=length(datafiles_list),  byrow=TRUE))
+  df$col2 <- read_success
+
+  # Add column names to new data frame
+  colnames(df) <- c("File name", "Successfully read")
+
+  # Export as text file - always
+
+  txtfile = here(paste0(basename,".txt"))
+  xlsxfile= here(paste0(basename,".xlsx"))
+
+  message(paste0("Writing out results: ",txtfile))
+  write.table(df, file = txtfile, 
+                sep = ",", 
+                quote = FALSE, 
+                row.names = FALSE)
+
+
+  # Export results as Excel if xlsx package installed
+  t <- try(library(xlsx))
+  if ("try-error" %in% class(t)) {
+    message("No xlsx library, skipping write-out of XLSX file.")
+
+  } else{
+    message(paste0("Writing out results: ",xlsxfile))
+    write.xlsx(df, file = xlsxfile, 
+                sheetName = "Output", 
+                append = FALSE)
+  }
+}
