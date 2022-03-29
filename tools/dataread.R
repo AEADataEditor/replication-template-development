4609e46d4d74fc0251c905890d639b3edaa0310e
aaf101 <100437375+aaf101@users.noreply.github.com>
2022-03-28 22:40:57 -0400

    Removed need for folder_list

--- tools/dataread.R
+++ tools/dataread.R
@@ -9,36 +9,25 @@ basename <- "r-data-checks.txt"
 # Finds current directory
 root <- here()
 
-# Retrieve list of all folders
-dir_list <- list.dirs(root)[-1]
-folder_list <- gsub("\\./", "", dir_list)
-
 # Create list of all RDS files in directory
-datafiles_list <- list()
-for(k in 1:length(folder_list)){
-  setwd(folder_list[[k]])
-  listrds <- dir(pattern = ".rds")
-  listRds <- dir(pattern = ".Rds")
-  listRDS <- dir(pattern = ".RDS")
-  datafiles_list <- append(datafiles_list, listrds)
-  datafiles_list <- append(datafiles_list, listRds)
-  datafiles_list <- append(datafiles_list, listRDS)
-}
+datafiles_list <- list.files(root, pattern = ".rds", full.names = TRUE, recursive = TRUE, ignore.case = TRUE, include.dirs = FALSE)
+
+# Create list denoting successes/failures
+read_success <- list()
 
 # Loop to read all RDS files, recording successes as "yes"
-dataFiles <- list()
 for(k in 1:length(datafiles_list)){
   t <- try(readRDS(datafiles_list[[k]]))
   if ("try-error" %in% class(t)){
-    datafiles_list[[k]][2] <- "No"
+    read_success[[k]] <- "No"
   } else{
-    datafiles_list[[k]][2] <- "Yes"
+    read_success[[k]] <- "Yes"
   }
-  dataFiles[[k]] <- ""
 }
 
 # Convert list into data frame
 df <- data.frame(matrix(unlist(datafiles_list), nrow=length(datafiles_list), byrow=TRUE))
+df$col2 <- read_success
 
 # Add column names to new data frame
 colnames(df) <- c("File name", "Successfully read?")
