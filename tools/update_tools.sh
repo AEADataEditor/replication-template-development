18223bf809a7f8edba1a7b3e71b64adb3f836c17
Lars Vilhuber <lars@vilhuber.com>
2023-01-10 14:36:08 -0500

    Adding a tool to update the tools

---
+++ tools/update_tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -O master.zip https://github.com/AEADataEditor/replication-template/archive/refs/heads/master.zip
+unzip master.zip 
+cd replication-template-master
+[[ -f config.yml ]] && mv config.yml config-template.yml
+[[ -f requirements.txt ]] && mv requirements.txt requirements-template.yml
+tar cvf ../tmp.tar tools/ automations/ *.yml template-* 
+cd ..
+tar xvf tmp.tar
+git add tools/ automations/ *.yml template-* 
+git commit -m '[skip ci] Update of tools'
+\rm -rf replication-template-master tmp.tar master.zip
