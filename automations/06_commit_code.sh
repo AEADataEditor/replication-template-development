edf4c2c035bc199522992a4fe297bbee8f4329be
Lars Vilhuber <lars@vilhuber.com>
2023-01-26 15:58:19 -0500

    One more robustness edit

--- automations/06_commit_code.sh
+++ automations/06_commit_code.sh
@@ -6,7 +6,7 @@ set -ev
 if [ ! -z $1 ] 
 then 
   git add $1
-  git commit -m "[skipci] Adding code from $1" $1
+  git commit -m "[skipci] Adding code from $1" $1 | tee aux/git-commit.log
   case $? in
      0)
      echo "Code added"
