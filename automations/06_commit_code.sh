90e7518a327463b717a8af45b301de5668f4908d
Lars Vilhuber <lars@vilhuber.com>
2023-01-28 20:20:47 -0500

    Adding tags to code commits

--- automations/06_commit_code.sh
+++ automations/06_commit_code.sh
@@ -7,9 +7,13 @@ if [ ! -z $1 ]
 then 
   git add $1
   git commit -m "[skipci] Adding code from $1" $1 | tee aux/git-commit.log
-  case $? in
+  case ${PIPESTATUS[0]} in
      0)
      echo "Code added"
+     # count the number of previous tags
+     tags=$(git tag| wc -l)
+     let tags++
+     git tag -m "Code added from ICPSR" update$tags
      ;;
      1)
      echo "No changes detected"
