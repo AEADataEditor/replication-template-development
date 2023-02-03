bcdbf9cb83acec7e792293263074047a4895cb21
Lars Vilhuber <lars@vilhuber.com>
2023-02-02 21:17:46 -0500

    Adding an optional argument
    This way, the script could also be run interactively.

--- automations/20_amend_report.sh
+++ automations/20_amend_report.sh
@@ -3,9 +3,19 @@ set -ev
 
 [[ "$SkipProcessing" == "yes" ]] && exit 0
 
+[[ -z $1 ]] && indir=aux || indir=$@
+
+if [ ! -d "$indir" ]
+then
+   echo "$indir is not a directory"
+   echo "Please check, and if necessary, call this script"
+   echo "as $0 [name of dir]"
+   exit 2
+fi
+
 # if necessary, install the requirements
 if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
 
 
 # Now use the template to fill it in
-python3 tools/replace_placeholders.py --indir aux --outfile aux/REPLICATION-filled.md
+python3 tools/replace_placeholders.py --indir "$indir" --outfile "$indir/REPLICATION-filled.md"
