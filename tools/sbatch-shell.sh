d6b34e9263f684fe7a85c7880860d7870e16ce73
Lars Vilhuber <lars@vilhuber.com>
2023-05-16 11:52:28 -0400

    Update sbatch-shell.sh

--- tools/sbatch-shell.sh
+++ tools/sbatch-shell.sh
@@ -14,5 +14,13 @@
 # Wall clock limit:
 #SBATCH --time=00:00:30
 #
+# Email?
+# Probably do not need "--mail-user=youremail@cornell.edu"
+# Just add your email to the file "$HOME/.forward"
+# 
+#SBATCH --mail-type=ALL
+#
 ## Command(s) to run (example):
 /usr/local/stata16/stata-mp -b main.do
+## Matlab - will run "main.m", output goes to "srun-NNNN.out"
+# matlab -nodisplay -r "addpath(genpath('.')); main"
