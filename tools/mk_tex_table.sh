34e8e73b215be52817078dbe3bac4de7f02630f2
Lars Vilhuber <lars@vilhuber.com>
2023-05-21 10:39:05 -0400

    Better latex conversion

--- tools/mk_tex_table.sh
+++ tools/mk_tex_table.sh
@@ -3,11 +3,14 @@ for arg in $(ls *tex | grep -v pdf_)
 do
   echo "\documentclass{article}
 	\usepackage[utf8]{inputenc}
+	\usepackage{eurosym}
 	\usepackage{graphicx}
 	\usepackage[landscape,margin=0.5in]{geometry}
         \usepackage{hyperref}
 	\usepackage{xcolor}
 	\usepackage{subfig}
+	\usepackage{caption}
+	\usepackage{booktabs}
 	\begin{document}
 	" > pdf_$arg
 	cat $arg >> pdf_$arg
