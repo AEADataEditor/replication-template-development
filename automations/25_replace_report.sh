9a80f2a8bd314b36d99deedefc71eb53a62b1cee
Lars Vilhuber <lars@vilhuber.com>
2024-01-12 23:21:50 +0000

    Adding additional steps

--- automations/25_replace_report.sh
+++ automations/25_replace_report.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ev
 
+report=REPLICATION.md
+parta=REPLICATION-PartA.md
+partb=REPLICATION-PartB.md
 
 
 [[ "$SkipProcessing" == "yes" ]] && exit 0
@@ -19,20 +22,20 @@ then
     sha256sum -c generated/REPLICATION.sha256 || exit 0
     case $? in
     0)
-    echo "Replacing REPLICATION.md"
-    mv generated/REPLICATION-filled.md REPLICATION.md
-    git add REPLICATION.md
-    git commit -m '[skipci] Updated report' REPLICATION.md
-    # splitting the report
-    splitline=$(grep -n "You have now completed" REPLICATION.md | cut -f1 -d:)
-    head -n $splitline REPLICATION.md > REPLICATION-partA.md
-    tail -n +$(($splitline + 1)) REPLICATION.md > REPLICATION-partB.md
+    echo "Replacing $report"
+    mv generated/REPLICATION-filled.md $report
+    git add $report
+    git commit -m '[skipci] Updated report' $report
+    # splitting the report - NEW in 2024
+    splitline=$(grep -n "You are starting \*PartB\*." $report | cut -f1 -d:)
+    head -n $(( splitline - 1))  $report > REPLICATION-partA.md
+    tail -n +$splitline          $report > REPLICATION-partB.md
     git add REPLICATION-partA.md REPLICATION-partB.md
     git commit -m '[skipci] Added split report' REPLICATION-part?.md
     exit 0
     ;;
     *)
-    echo "Not replacing REPLICATION.md - appears to be different"
+    echo "Not replacing $report - appears to be different"
     echo "Verify generated/REPLICATION-filled.md"
     exit 0
     ;;
--- build/build-split-report.sh
+++ build/build-split-report.sh
@@ -7,15 +7,8 @@ partb=REPLICATION-PartB.md
 
 # start the process
 
-grep -n      "You are starting *PartB*."             $report >> $extreport
-grep -A 11   "#### All data files provided"    $report | \
-             sed 's+####+###+'                         >> $extreport
-grep -A 13   "### Analysis Data Files"         $report >> $extreport
-grep -A 79   "## Stated Requirements"          $report >> $extreport
-grep -A 110  "## Replication steps"            $report >> $extreport
+splitline=$(grep -n      "You are starting \*PartB\*." $report | awk -F: '{print $1}')   
 
-# remove the "Workflow stage" instructions
-
-mv $extreport tmp-$extreport
-grep -v "Workflow stage:" tmp-$extreport > $extreport
+head -n $(( splitline - 1))  $report >> $parta
+tail -n +$splitline          $report >> $partb
 
