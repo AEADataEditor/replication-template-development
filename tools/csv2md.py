5e76a20c9e7db1f0cae29bd5ab9919db8ccca57a
Lars Vilhuber <lars@vilhuber.com>
2024-02-01 13:29:35 -0500

    Adding additional checks for non-UTF-8 scenarios

--- tools/csv2md.py
+++ tools/csv2md.py
@@ -10,9 +10,16 @@ def try_utf8(filehdl):
     "Returns a Unicode object on success, or None on failure"
     try:
        data = filehdl.read()
-       return data.decode('utf-8')
     except UnicodeDecodeError:
        return None
+    
+    if not data:
+        return None
+    
+    try:
+        return data.decode('utf-8')
+    except UnicodeDecodeError:
+        return None
 
 def blank_md(md_file,message):
     # CSV is empty, write message to Markdown file
@@ -21,7 +28,7 @@ def blank_md(md_file,message):
         exit()
 
 def csv_to_markdown(csv_file, md_file):
-    with open(csv_file, 'r') as f:
+    with open(csv_file, 'rb') as f:
         utfdata = try_utf8(f)
         if utfdata is None:
             print("Error: CSV file is not UTF-8 encoded")
@@ -30,8 +37,8 @@ def csv_to_markdown(csv_file, md_file):
             reader = csv.DictReader(f)
             rows = list(reader)
             if not rows:
-                print("Error: CSV file is empty")
-                blank_md(md_file,"No data found")
+                print("NOTE: CSV file is empty, Probably no PII found")
+                blank_md(md_file,"No PII data found.")
 
     # All exceptions should be handled, lets continue
 
