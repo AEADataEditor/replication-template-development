f48ca9ba616858b5350068a4ce04ee1093651f43
Lars Vilhuber <lars@vilhuber.com>
2025-01-10 12:24:00 -0500

    Updating the sbatch shell

--- tools/sbatch-shell.sh
+++ tools/sbatch-shell.sh
@@ -14,7 +14,7 @@
 # Processors per task: here, 8 bc we have Stata-MP/8
 #SBATCH --cpus-per-task=8
 #
-# Wall clock limit:
+# Wall clock limit: adjust accordingly. Format is HH:MM:SS or DD-HH:MM:SS where DD are days.
 #SBATCH --time=00:00:30
 #
 # Email?
@@ -25,5 +25,5 @@
 #
 ## Command(s) to run (example):
 /usr/local/stata16/stata-mp -b main.do
-## Matlab - will run "main.m", output goes to "srun-NNNN.out"
-# matlab -nodisplay -r "addpath(genpath('.')); main"
+## Matlab - will run "main.m", output to "main.log"
+# matlab -nodisplay -r "addpath(genpath('.')); main" -logfile main.log
