43c42cbedf96d0defaed74adca23248027f4d9c0
Lars Vilhuber <lars.vilhuber@cornell.edu>
2026-04-20 08:12:00 -0400

    Added cpp and h as programming file extensions

--- automations/03_list_program_files.sh
+++ automations/03_list_program_files.sh
@@ -23,7 +23,7 @@ then
   mkdir generated
 fi
 
-extensions="ado do r rmd qmd ox m py nb ipynb sas jl f f90 c c++ sh toml yaml yml toml fs fsx gss"
+extensions="ado do r rmd qmd ox m py nb ipynb sas jl f f90 c c++ sh toml yaml yml toml fs fsx gss cpp h"
 # these usually do not have extensions
 fullnames="makefile"
 
